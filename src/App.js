54a4c0a99786c5c5178c67679bb151e732ef93ab
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-21 15:04:32 +0100

    added MenuItem

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'
 import Paper from './components/Paper'
-import Counter from './Counter'
+import MenuItem from 'material-ui/MenuItem'
 
+import Counter from './Counter'
 import Navigation from './Navigation/Navigation'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
@@ -16,14 +17,19 @@ const App = (props) => (
         <Navigation
           label={"JFDDL6"}
         >
-          <Link to="/">Dashboard</Link>
-          <Link to="/counter">Counter</Link>
+          <Link to="/">
+          <MenuItem>
+          Dashboard
+          </MenuItem>
+          </Link>
+          <Link to="/counter">
+          <MenuItem>
+          Counter
+          </MenuItem>
+          </Link>
         </Navigation>
-
-       
           <Route path="/Counter" component={() =>  <Paper><Counter startNumber={5} /> </Paper>}
           />
-       
       </div>
     </Router>
 
