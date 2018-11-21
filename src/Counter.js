99639ec66b749c2fb9e34d1da2b4833917b41c30
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-21 12:21:16 +0100

    added reseet button

--- src/Counter.js
+++ src/Counter.js
@@ -2,16 +2,21 @@ import React from 'react'
 import Button from './components/Button'
 
 class Counter extends React.Component {
-        state = {
-            number: this.props.startNumber
-        }
+    state = {
+        number: this.props.startNumber
+    }
 
-incHandler = () => {
-    this.setState({ number: this.state.number + 1 })
-}
-decHandler= () => {
-    this.setState({ number: this.state.number - 1 })
-}
+    incHandler = () => {
+        this.setState({ number: this.state.number + 1 })
+    }
+    decHandler = () => {
+        this.setState({ number: this.state.number - 1 })
+    }
+    resetHandler = () => {
+        this.setState({ number: this.props.startNumber})
+    }
+    
+
 
     render() {
         return (
@@ -26,6 +31,10 @@ decHandler= () => {
                 <Button
                     onClick={this.decHandler}
                     label={'-'}
+                />
+                <Button
+                    onClick={this.resetHandler}
+                    label={'reset'}
                 />
             </div>
 
