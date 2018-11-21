128ebc71586b35c5ff98325d7c50eaed99079d0f
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-21 14:05:03 +0100

    added sidebar

--- src/Navigation/Navigation.js
+++ src/Navigation/Navigation.js
@@ -1,11 +1,29 @@
 import React from 'react'
+
 import AppBar from 'material-ui/AppBar'
-const Navigation = (props) => (
-    <div>
-        <AppBar
-            title="Title"
-            iconClassNameRight="muidocs-icon-navigation-expand-more"
-        />
-    </div>
-)
+import Drawer from 'material-ui/Drawer';
+
+class Navigation extends React.Component {
+    state = {
+        isDrawerOpen: false
+    }
+    toggleDrawer = () => this.setState({ isDrawerOpen: !this.state.isDrawerOpen })
+    render() {
+        return (
+            <div>
+                <AppBar
+                    title="JFDDL6"
+                    onLeftIconButtonClick={this.toggleDrawer}
+                />
+                <Drawer
+                    docked={false}
+                    width={200}
+                    open={this.state.isDrawerOpen}
+                    onRequestChange={this.toggleDrawer}
+                >
+                </Drawer>
+            </div >
+        )
+    }
+}
 export default Navigation
