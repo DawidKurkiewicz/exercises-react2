e5a91f5e65ca4d6ef82104cbfff86ab23fac2dd3
dawidkurkiewicz <dawidkurkiewicz516@gmail.com>
2018-11-21 13:29:27 +0100

    added appbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import Paper from './components/Paper'
 import Counter from './Counter'
+import Navigation from './Navigation/Navigation'
 
 
 const App = (props) => (
-  <Paper>
-    <Counter
-      startNumber={5}
-    />
-  </Paper>
+  <div>
+    <Navigation />
+    <Paper>
+      <Counter
+        startNumber={5}
+      />
+    </Paper>
+  </div>
 )
 export default App
